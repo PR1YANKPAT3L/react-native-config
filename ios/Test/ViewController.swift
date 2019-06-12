797141f828c74176a0271ba86e300c02efe2f71b
doozMen <stijn@dooz.be>
2019-06-12 12:54:45 +0200

    M (367, 68) Secrets.swift commit(in:) added and committed secrets /Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Coder.xcconfig.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.debug.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.release.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.betaRelease.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/coder.env.json.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.h.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.m.gpg.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Coder.xcconfig.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.debug.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.release.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.betaRelease.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/coder.env.json.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.h.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.m.secret.gpg,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Coder.xcconfig.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.debug.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.release.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.betaRelease.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/coder.env.json.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.h.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.m.gpg.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Coder.xcconfig.secret.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.debug.secret.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.release.secret.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/android/.env.betaRelease.secret.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/coder.env.json.secret.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.h.secret.secret,/Users/stijnwillems/Documents/dev.nosync/Open/react-native-config/ios/Sources/RNConfigurationBridge/ReactNativeConfig.m.secret.secret

---
+++ ios/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Stijn Willems on 12/06/2019.
+//  Copyright © 2019 Pedro Belo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
