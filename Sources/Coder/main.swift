d94433862e104e61890f3be38f25f388b0ac0762
doozMen <stijn@dooz.be>
2019-06-13 13:33:56 +0200

    works from a copy

--- Sources/Coder/main.swift
+++ Sources/Coder/main.swift
@@ -28,9 +28,13 @@ doContinue(pretty_function() + " setup")
 
     let input = try srcRoot.file(named: "coder.env.json")
 
+    // let packageCoderSources = highway.dependency(with: "Coder").path
+    // in your project use packageCoderSources from above, faking it here
     let output = try CoderOutput(packageCoderSources: srcRoot, xcodeProjectName: xcodeName)
     let copy = Copy(output: output)
     try copy.attempt(to: srcRoot, xcodeProjectName: "Coder")
+
+    // faking the copy, back to srcRoot
 
     copiedPackageCoderSources = try CoderOutput(packageCoderSources: srcRoot, xcodeProjectName: xcodeName)
 
